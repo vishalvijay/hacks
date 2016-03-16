a48658d7e56a6c45b7c434925e7ba2b48d39fa42
Vishal Vijay <vishal.vijay@peppertap.com>
2016-03-16 23:52:50 +0530

    Add new alphabets problem

---
+++ new_alphabets/a.rb
@@ -0,0 +1,35 @@
+require 'set'
+input = ["bfgh", "cbf", "gh", "edg", "ag", "edc", "ae", "dbg"]
+# input = ["ba", "cea", "de", "efa", "bcd"]
+alps = {}
+input.each do |str|
+	str.split("").each_with_index do |char, i|
+		alps[char] = {char: char[0], left: Set.new, right: Set.new} unless alps[char]
+		alps[char][:left].merge str[0..i-1].split("") if i > 0
+		alps[char][:right].merge str[i+1..-1].split("") if i < str.length-1
+	end
+end
+final_alps = alps.values.sort do |a, b|
+	a[:left].length <=> b[:left].length
+end
+result = []
+final_alps.each do |alp|
+	result << alp[:char]
+end
+i = final_alps.length-1
+while(i >= 0) do
+	alp = alps[result[result.length-1-i]]
+	swapped = false
+	alp[:right].each do |char|
+		j = result.index(char)
+		if result.length-1-i > j
+			result[j] = result[result.length-1-i]
+			result[result.length-1-i] = char
+			swapped = true
+			break
+		end
+	end
+	i -= 1 unless swapped
+end
+
+puts result.join
---
+++ new_alphabets/q.md
@@ -0,0 +1,21 @@
+Build new alphabets order based on given dictionary
+---------------------------------------------------
+bfgh
+cbf
+gh
+edg
+ag
+edc
+ae
+dbg
+
+
+Result: aedcbfgh
+
+ba
+cea
+de
+efa
+bcd
+
+Result: bcdefa
