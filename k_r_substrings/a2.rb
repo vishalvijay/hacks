7c2173e1bd00cae971ee77c490dd672b40357029
Vishal Vijay <vishal.vijay@peppertap.com>
2016-03-19 02:50:34 +0530

    Fix k_r_substring a2

--- k_r_substrings/a2.rb
+++ k_r_substrings/a2.rb
@@ -6,34 +6,27 @@ t = gets.chomp.to_i
 	str = gets.chomp
 	m_sub_count = 0
 	n_sub_count = 0
-	count_array = []
 	r = []
 	k = []
-	(0...str.length).each do |i|
-		if str[str.length-1-i] == "K"
-			k = [str.length-1-i] + k
-		else
-			r = [str.length-1-i] + r
-		end
-		count_array[str.length-1-i] = {r: r, k: k}
-	end
-	(0...str.length).each do |i|
-		if count_array[i][:r].length >= m
+	(0...str.length).each do |index|
+		i = str.length-1-index
+		str[i] == "K" ? k.unshift(i) : r.unshift(i)
+		if r.length >= m
 			if m == 0
-				m_sub_count += count_array[i][:r][m] ? count_array[i][:r][m] - i : str.length - i
-			elsif m < count_array[i][:r].length
-				m_sub_count += count_array[i][:r][m] - count_array[i][:r][m-1]
+				m_sub_count += r[m] ? r[m] - i : str.length - i
+			elsif m < r.length
+				m_sub_count += r[m] - r[m-1]
 			else
-				m_sub_count += str.length - count_array[i][:r][m-1]
+				m_sub_count += str.length - r[m-1]
 			end
 		end
-		if count_array[i][:k].length >= n
+		if k.length >= n
 			if n == 0
-				n_sub_count += count_array[i][:k][n] ? count_array[i][:k][n] - i : str.length - i
-			elsif n < count_array[i][:k].length
-				n_sub_count += count_array[i][:k][n] - count_array[i][:k][n-1]
+				n_sub_count += k[n] ? k[n] - i : str.length - i
+			elsif n < k.length
+				n_sub_count += k[n] - k[n-1]
 			else
-				n_sub_count += str.length - count_array[i][:k][n-1]
+				n_sub_count += str.length - k[n-1]
 			end
 		end
 	end
