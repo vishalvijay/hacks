f74f15e9b368126ff4f25200ebebd17fb26be4ad
Vishal Vijay <vishal.vijay@peppertap.com>
2016-04-26 02:00:13 +0530

    Bug fix message encryption

--- message_encryption /a.rb
+++ message_encryption /a.rb
@@ -3,7 +3,7 @@ def decrypt_message(encrypted_message, number_of_columns)
 	r += 1 if encrypted_message.length%number_of_columns > 0
 	result = ""
 	(0...number_of_columns).each do |i|
-		(0...7).each do |j|
+		(0...r).each do |j|
 			index = (j % 2 == 0 ? i + (j * number_of_columns) : (j * number_of_columns + number_of_columns-1) - i)
 			result += encrypted_message[index] if encrypted_message[index]
 		end
